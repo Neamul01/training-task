05a81ef0d581dedbad0c94fbf7ae0fc2740c2b4e
Neamul01 <neamulhaque2002@gmail.com>
2023-06-24 16:04:00 +0600

    tab added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // State to keep track of the active tab
+  const [activeTab, setActiveTab] = useState("allOrders");
+
+  // Function to handle tab click
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  // Content for each tab
+  const tabContent = {
+    allOrders: <div>All Orders</div>,
+    regularDelivery: <div>Regular Delivery</div>,
+    expressDelivery: <div>Express Delivery</div>,
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="inner-container">
+          <div>
+            <div>
+              {/* Tab buttons */}
+              <button
+                onClick={() => handleTabClick("allOrders")}
+                className={`${
+                  activeTab === "allOrders" ? "active-button" : "general-button"
+                } order-tab`}
+              >
+                All Orders
+              </button>
+              <button
+                onClick={() => handleTabClick("regularDelivery")}
+                className={`${
+                  activeTab === "regularDelivery"
+                    ? "active-button"
+                    : "general-button"
+                } order-tab`}
+              >
+                Regular Delivery
+              </button>
+              <button
+                onClick={() => handleTabClick("expressDelivery")}
+                className={`${
+                  activeTab === "expressDelivery"
+                    ? "active-button"
+                    : "general-button"
+                } order-tab`}
+              >
+                Express Delivery
+              </button>
+            </div>
+
+            <div>
+              {/* Selected tab content */}
+              {tabContent[activeTab]}
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
